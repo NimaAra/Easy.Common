aae10db1229c40ae8043750f1da5016e4a1bd7e1
Nima Ara <nima.ara@inbox.com>
2016-08-29 14:13:59 +0100

    Bug fix in the UriExtensions class

--- Easy.Common/Extensions/UriExtensions.cs
+++ Easy.Common/Extensions/UriExtensions.cs
@@ -14,13 +14,11 @@
         /// <summary>
         /// Extracts Parameters and Values from the Query String
         /// </summary>
-        /// <param name="uri"></param>
-        /// <returns>Read Only Dictionary of Parameters and Values</returns>
         public static IEnumerable<KeyValuePair<string, string>> ParseQueryString(this Uri uri)
         {
             Ensure.NotNull(uri, nameof(uri));
 
-            var match = QueryStringRegex.Match(uri.PathAndQuery);
+            var match = QueryStringRegex.Match(uri.OriginalString);
             while (match.Success)
             {
                 yield return new KeyValuePair<string, string>(match.Groups[1].Value, match.Groups[2].Value);
@@ -29,12 +27,8 @@
         }
 
         /// <summary>
-        /// Adds or Appends parameters and values to the Query String
+        /// Adds or appends parameters and values to the query-string.
         /// </summary>
-        /// <param name="uri"></param>
-        /// <param name="parameter"></param>
-        /// <param name="value"></param>
-        /// <returns>New Uri with the Query String</returns>
         public static Uri AddParametersToQueryString(this Uri uri, string parameter, string value)
         {
             Ensure.NotNull(uri, nameof(uri));
@@ -46,24 +40,16 @@
         }
 
         /// <summary>
-        /// Adds or Appends parameters and values to the Query String
+        /// Adds or appends parameters and values to the query-string.
         /// </summary>
-        /// <param name="uri"></param>
-        /// <param name="parameter"></param>
-        /// <param name="value"></param>
-        /// <returns>New Uri with the Query String</returns>
         public static Uri AddParametersToQueryString(this Uri uri, string parameter, int value)
         {
             return uri.AddParametersToQueryString(parameter, (long)value);
         }
 
         /// <summary>
-        /// Adds or Appends parameters and values to the Query String
+        /// Adds or appends parameters and values to the query-string.
         /// </summary>
-        /// <param name="uri"></param>
-        /// <param name="parameter"></param>
-        /// <param name="value"></param>
-        /// <returns>New Uri with the Query String</returns>
         public static Uri AddParametersToQueryString(this Uri uri, string parameter, long value)
         {
             return uri.AddParametersToQueryString(parameter, value.ToString());
@@ -72,7 +58,6 @@
         /// <summary>
         /// Applies the specified <paramref name="modification"/> to the <paramref name="uri"/> query-string.
         /// </summary>
-        /// <returns>An absolute URI with modified query-string</returns>
         public static string WithModifiedQuerystring(this Uri uri, Action<IEnumerable<KeyValuePair<string, string>>> modification)
         {
             Ensure.NotNull(uri, nameof(uri));
