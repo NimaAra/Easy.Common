ff8c66c63bc3e3264701dd2791a63f915bf3f59b
Nima Ara <nima.ara@inbox.com>
2016-08-29 13:00:01 +0100

    Added byte extensions

---
+++ Easy.Common.Tests.Unit/ByteExtensions/ComparingByteArraysTests.cs
@@ -0,0 +1,51 @@
+﻿namespace Easy.Common.Tests.Unit.ByteExtensions
+{
+    using System;
+    using Easy.Common.Extensions;
+    using NUnit.Framework;
+    using Shouldly;
+
+    [TestFixture]
+    internal sealed class ComparingByteArraysTests
+    {
+        [Test]
+        public void When_comparing_byte_arrays()
+        {
+            var left = new byte[] {1, 2, 3};
+            var right = new byte[] {1, 2, 3};
+
+            left.Compare(left).ShouldBeTrue();
+
+            left.Compare(right).ShouldBeTrue();
+            right.Compare(left).ShouldBeTrue();
+
+            right = new byte[] {1, 3, 2};
+            left.Compare(right).ShouldBeFalse();
+            right.Compare(left).ShouldBeFalse();
+
+            right = new byte[] { 1, 2, 3 };
+            left.Compare(right).ShouldBeTrue();
+            right.Compare(left).ShouldBeTrue();
+
+            right = new byte[] { 1, 2, 3, 4 };
+            left.Compare(right).ShouldBeFalse();
+            right.Compare(left).ShouldBeFalse();
+
+            left = null;
+            Should.Throw<ArgumentNullException>(() => left.Compare(right))
+                .Message.ShouldBe("Value cannot be null.\r\nParameter name: left");
+
+            left = new byte[] {1};
+            right = null;
+
+            Should.Throw<ArgumentNullException>(() => left.Compare(right))
+                .Message.ShouldBe("Value cannot be null.\r\nParameter name: right");
+
+            left = new byte[0];
+            right = new byte[0];
+
+            left.Compare(right).ShouldBeTrue();
+            right.Compare(left).ShouldBeTrue();
+        }
+    }
+}
---
+++ Easy.Common/Extensions/ByteExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace Easy.Common.Extensions
+{
+    using System.Runtime.InteropServices;
+
+    /// <summary>
+    /// Provides a set of helper methods for working with <see cref="byte"/>.
+    /// </summary>
+    public static class ByteExtensions
+    {
+        [DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern int memcmp(byte[] b1, byte[] b2, long count);
+
+        /// <summary>
+        /// Compares the given <paramref name="left"/> with <paramref name="right"/>.
+        /// </summary>
+        /// <returns><c>True</c> if the two are equal otherwise <c>False</c></returns>
+        public static bool Compare(this byte[] left, byte[] right)
+        {
+            Ensure.NotNull(left, nameof(left));
+            Ensure.NotNull(right, nameof(right));
+            
+            // Validate buffers are the same length.
+            // This also ensures that the count does not exceed the length of either buffer.  
+            return left.Length == right.Length && memcmp(left, right, left.Length) == 0;
+        }
+    }
+}
