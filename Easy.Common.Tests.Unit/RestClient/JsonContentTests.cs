b98b5d1aea6c5ed49951682da4b3128bef52de84
Nima Ara <nima.ara@inbox.com>
2016-11-08 23:44:38 +0000

    Minor Awesomeness!
    * Added JSONContent;
    * Added XMLContent.

---
+++ Easy.Common.Tests.Unit/RestClient/JsonContentTests.cs
@@ -0,0 +1,42 @@
+﻿namespace Easy.Common.Tests.Unit.RestClient
+{
+    using System.Net.Http.Headers;
+    using System.Text;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+    using Shouldly;
+
+    [TestFixture]
+    internal sealed class JSONContentTests
+    {
+        [Test]
+        public async Task When_creating_with_default_constructor()
+        {
+            var content = new JSONContent("Foo");
+
+            content.Headers.ShouldNotBeNull();
+            content.Headers.ShouldHaveSingleItem();
+
+            content.Headers.ContentType
+                .ShouldBe(MediaTypeHeaderValue.Parse("application/json; charset=utf-8"));
+
+            var json = await content.ReadAsStringAsync();
+            json.ShouldBe("Foo");
+        }
+
+        [Test]
+        public async Task When_creating_with_given_encoding()
+        {
+            var content = new JSONContent("Foo", Encoding.UTF32);
+
+            content.Headers.ShouldNotBeNull();
+            content.Headers.ShouldHaveSingleItem();
+
+            content.Headers.ContentType
+                .ShouldBe(MediaTypeHeaderValue.Parse("application/json; charset=utf-32"));
+
+            var json = await content.ReadAsStringAsync();
+            json.ShouldBe("Foo");
+        }
+    }
+}
---
+++ Easy.Common.Tests.Unit/RestClient/XMLContentTests.cs
@@ -0,0 +1,42 @@
+﻿namespace Easy.Common.Tests.Unit.RestClient
+{
+    using System.Net.Http.Headers;
+    using System.Text;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+    using Shouldly;
+
+    [TestFixture]
+    internal sealed class XMLContentTests
+    {
+        [Test]
+        public async Task When_creating_with_default_constructor()
+        {
+            var content = new XMLContent("Foo");
+
+            content.Headers.ShouldNotBeNull();
+            content.Headers.ShouldHaveSingleItem();
+
+            content.Headers.ContentType
+                .ShouldBe(MediaTypeHeaderValue.Parse("application/xml; charset=utf-8"));
+
+            var json = await content.ReadAsStringAsync();
+            json.ShouldBe("Foo");
+        }
+
+        [Test]
+        public async Task When_creating_with_given_encoding()
+        {
+            var content = new XMLContent("Foo", Encoding.UTF32);
+            
+            content.Headers.ShouldNotBeNull();
+            content.Headers.ShouldHaveSingleItem();
+
+            content.Headers.ContentType
+                .ShouldBe(MediaTypeHeaderValue.Parse("application/xml; charset=utf-32"));
+
+            var json = await content.ReadAsStringAsync();
+            json.ShouldBe("Foo");
+        }
+    }
+}
