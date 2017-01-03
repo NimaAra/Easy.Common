e45959b2f5cfaa35627fc7258488b45ba82b2de9
Nima Ara <nima.ara@inbox.com>
2017-01-03 21:45:42 +0000

    Nothing important
    Updated copyright.

--- Version.cs
+++ Version.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("EasyCommon")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 
 [assembly: AssemblyVersion("1.5.1.0")]
 [assembly: AssemblyFileVersion("1.5.1.0")]
