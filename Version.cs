76c721dd9ad3afd5490f30d11c1a35bc1d25ff04
Nima Ara <nima.ara@inbox.com>
2017-02-13 18:31:54 +0000

    Updated version.

--- Version.cs
+++ Version.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Easy.Common")]
 [assembly: AssemblyCopyright("Copyright © 2017")]
 
-[assembly: AssemblyVersion("1.8.2.0")]
-[assembly: AssemblyFileVersion("1.8.2.0")]
+[assembly: AssemblyVersion("1.8.3.0")]
+[assembly: AssemblyFileVersion("1.8.3.0")]
