cac1864d7fd5e0ebab31f97e0d0745646f496c21
NimaAra <Nima.Ara@outlook.com>
2017-06-29 20:04:02 +0100

    Minor bug fix and added new overload

--- Easy.Common/ProcessHelper.cs
+++ Easy.Common/ProcessHelper.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Diagnostics;
+    using System.IO;
     using System.Threading.Tasks;
     using Easy.Common.Extensions;
 
@@ -11,11 +12,11 @@
     public static class ProcessHelper
     {
         /// <summary>
-        /// Starts a Process Asynchronously;
-        /// http://www.levibotelho.com/development/async-processes-with-taskcompletionsource/
+        /// Starts a Process Asynchronously.
+        /// <remarks><see href="http://www.levibotelho.com/development/async-processes-with-taskcompletionsource/"/></remarks>
         /// </summary>
-        /// <param name="processInfo">The information for the process to run</param>
-        /// <returns>A task representing the started process</returns>
+        /// <param name="processInfo">The information for the process to run.</param>
+        /// <returns>A task representing the started process which you can await until process exits.</returns>
         public static Task RunProcessAsync(ProcessStartInfo processInfo)
         {
             Ensure.NotNull(processInfo, nameof(processInfo));
@@ -25,47 +26,33 @@
             processInfo.RedirectStandardOutput = true;
             processInfo.RedirectStandardError = true;
 
-            var tcs = new TaskCompletionSource<object>();
             var process = new Process
             {
                 EnableRaisingEvents = true,
                 StartInfo = processInfo
             };
 
-            process.OutputDataReceived += (sender, args) =>
-            {
-                if (args?.Data == null) { return; }
-                args.Data.Print();
-            };
-            process.ErrorDataReceived += (sender, args) =>
-            {
-                var errMsg = $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}] - [Process Error] | ";
-
-                if (args?.Data != null)
-                {
-                    errMsg += args.Data;
-                }
-                else
-                {
-                    errMsg += "No data available";
-                }
-
-                errMsg.Print();
-            };
-
+            var tcs = new TaskCompletionSource<object>();
             process.Exited += (sender, args) =>
             {
-                if (process.ExitCode != 0)
+                var p = (Process)sender;
+
+                p.OutputDataReceived -= OnOutputDataReceived;
+                p.ErrorDataReceived -= OnErrorDataReceived;
+
+                if (p.ExitCode != 0)
                 {
                     tcs.TrySetException(new InvalidOperationException("The process did not exit gracefully. Search for [Process Error]"));
-                }
-                else
+                } else
                 {
                     tcs.SetResult(null);
                 }
 
-                process.Dispose();
+                p.Dispose();
             };
+
+            process.OutputDataReceived += OnOutputDataReceived;
+            process.ErrorDataReceived += OnErrorDataReceived;
 
             process.Start();
             process.BeginOutputReadLine();
@@ -74,15 +61,52 @@
         }
 
         /// <summary>
-        /// Starts a process represented by <paramref name="processPath"/> asynchronously
+        /// Starts a process represented by <paramref name="processPath"/> asynchronously.
+        /// <remarks><see href="http://www.levibotelho.com/development/async-processes-with-taskcompletionsource/"/></remarks>
         /// </summary>
-        /// <param name="processPath">The path to the process</param>
-        /// <returns>A task representing the started process</returns>
+        /// <param name="processPath">The path to the process.</param>
+        /// <returns>A task representing the started process which you can await until process exits.</returns>
         public static Task RunProcessAsync(string processPath)
         {
             Ensure.NotNullOrEmptyOrWhiteSpace(processPath);
             return RunProcessAsync(new ProcessStartInfo(processPath));
         }
-    }
 
+        /// <summary>
+        /// Starts a process represented by <paramref name="processPath"/> and <paramref name="args"/> asynchronously.
+        /// <remarks><see href="http://www.levibotelho.com/development/async-processes-with-taskcompletionsource/"/></remarks>
+        /// </summary>
+        /// <param name="processPath">The path to the process.</param>
+        /// <param name="args">The arguments to be passed to the process.</param>
+        /// <returns>A task representing the started process which you can await until process exits.</returns>
+        public static Task RunProcessAsync(FileInfo processPath, string args)
+        {
+            Ensure.NotNull(processPath, nameof(processPath));
+            Ensure.NotNullOrEmptyOrWhiteSpace(args);
+            return RunProcessAsync(new ProcessStartInfo(processPath.FullName, args));
+        }
+
+        private static void OnErrorDataReceived(object sender, DataReceivedEventArgs args)
+        {
+            var errMsg = $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}] - [Process Error] | ";
+
+            if (args?.Data != null)
+            {
+                errMsg += args.Data;
+            } else
+            {
+                if (((Process)sender).HasExited)
+                { return; }
+
+                errMsg += "No data available";
+            }
+
+            errMsg.Print();
+        }
+
+        private static void OnOutputDataReceived(object o, DataReceivedEventArgs args)
+        {
+            args?.Data?.Print();
+        }
+    }
 }
