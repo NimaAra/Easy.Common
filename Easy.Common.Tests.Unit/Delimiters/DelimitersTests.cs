fe30eee4d651f081d48be95fc311edd7ae76181b
NimaAra <Nima.Ara@outlook.com>
2017-07-24 22:59:29 +0100

    Added Delimiters

---
+++ Easy.Common.Tests.Unit/Delimiters/DelimitersTests.cs
@@ -0,0 +1,22 @@
+﻿namespace Easy.Common.Tests.Unit.Delimiters
+{
+    using Easy.Common;
+    using NUnit.Framework;
+    using Shouldly;
+
+    [TestFixture]
+    internal sealed class DelimitersTests
+    {
+        [Test]
+        public void When_checking_delimiters()
+        {
+            Delimiters.Comma.ShouldBe(new[] { ',' });
+            Delimiters.Dot.ShouldBe(new[] { '.' });
+            Delimiters.SemiColon.ShouldBe(new[] { ';' });
+            Delimiters.Colon.ShouldBe(new[] { ':' });
+            Delimiters.Space.ShouldBe(new[] { ' ' });
+            Delimiters.Tab.ShouldBe(new[] { '\t' });
+            Delimiters.Pipe.ShouldBe(new[] { '|' });
+        }
+    }
+}
---
+++ Easy.Common/Delimiters.cs
@@ -0,0 +1,43 @@
+namespace Easy.Common
+{
+    /// <summary>
+    /// Contains most commonly used delimiters as <see cref="T:char[]"/>.
+    /// </summary>
+    public static class Delimiters
+    {
+        /// <summary>
+        /// Gets a delimiter as <c>','</c>.
+        /// </summary>
+        public static char[] Comma = { ',' };
+
+        /// <summary>
+        /// Gets a delimiter as <c>'.'</c>.
+        /// </summary>
+        public static char[] Dot = { '.' };
+
+        /// <summary>
+        /// Gets a delimiter as <c>';'</c>.
+        /// </summary>
+        public static char[] SemiColon = { ';' };
+
+        /// <summary>
+        /// Gets a delimiter as <c>':'</c>.
+        /// </summary>
+        public static char[] Colon = { ':' };
+
+        /// <summary>
+        /// Gets a delimiter as <c>' '</c>.
+        /// </summary>
+        public static char[] Space = { ' ' };
+
+        /// <summary>
+        /// Gets a delimiter as <c>'\t'</c>.
+        /// </summary>
+        public static char[] Tab = { '\t' };
+
+        /// <summary>
+        /// Gets a delimiter as <c>'|'</c>.
+        /// </summary>
+        public static char[] Pipe = { '|' };
+    }
+}
