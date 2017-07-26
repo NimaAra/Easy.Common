d67af043e1236e02686e6da3920e5615ccb79986
NimaAra <Nima.Ara@outlook.com>
2017-07-27 00:42:58 +0100

    Nothing important

---
+++ NuGet-Pack.bat
@@ -0,0 +1,5 @@
+@echo off
+set version=%1
+
+dotnet restore .\Easy.Common.XAML
+dotnet pack .\Easy.Common.XAML\Easy.Common.XAML.csproj -o ..\nupkgs -c Release /p:PackageVersion=%version% --include-symbols --include-source
