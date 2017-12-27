0054d62e0d652e24180aae636925316cd17807d1
NimaAra <Nima.Ara@outlook.com>
2017-12-27 18:50:11 +0000

    Improved Retry.

--- Easy.Common/TryAndRetry/Retry.cs
+++ Easy.Common/TryAndRetry/Retry.cs
@@ -12,62 +12,62 @@ namespace Easy.Common
     public static class Retry
     {
         /// <summary>
-        /// Retries the given <paramref name="action"/> in case of an exception of 
+        /// Retries the given <paramref name="func"/> in case of an exception of 
         /// type <typeparamref name="TEx"/>.
         /// <remarks>
         /// If the given <paramref name="delays"/> is not supplied then the given 
-        /// <paramref name="action"/> will be retried once.
+        /// <paramref name="func"/> will be retried once.
         /// </remarks>
         /// </summary>
-        public static Task On<TEx>(Action action, params TimeSpan[] delays) 
-            where TEx : Exception => OnImpl(action, delays, typeof(TEx));
+        public static Task On<TEx>(Func<Task> func, params TimeSpan[] delays) 
+            where TEx : Exception => OnImpl(func, delays, typeof(TEx));
 
         /// <summary>
-        /// Retries the given <paramref name="action"/> in case of any of the given exceptions specified by
+        /// Retries the given <paramref name="func"/> in case of any of the given exceptions specified by
         /// <typeparamref name="TEx1"/> and <typeparamref name="TEx2"/>.
         /// <remarks>
         /// If the given <paramref name="delays"/> is not supplied then the given 
-        /// <paramref name="action"/> will be retried once.
+        /// <paramref name="func"/> will be retried once.
         /// </remarks>
         /// </summary>
-        public static Task OnAny<TEx1, TEx2>(Action action, params TimeSpan[] delays)
-            where TEx1 : Exception where TEx2 : Exception => OnImpl(action, delays, typeof(TEx1), typeof(TEx2));
+        public static Task OnAny<TEx1, TEx2>(Func<Task> func, params TimeSpan[] delays)
+            where TEx1 : Exception where TEx2 : Exception => OnImpl(func, delays, typeof(TEx1), typeof(TEx2));
 
         /// <summary>
-        /// Retries the given <paramref name="action"/> in case of any of the given exceptions specified by
+        /// Retries the given <paramref name="func"/> in case of any of the given exceptions specified by
         /// <typeparamref name="TEx1"/>, <typeparamref name="TEx2"/> and <typeparamref name="TEx3"/>.
         /// <remarks>
         /// If the given <paramref name="delays"/> is not supplied then the given 
-        /// <paramref name="action"/> will be retried once.
+        /// <paramref name="func"/> will be retried once.
         /// </remarks>
         /// </summary>
-        public static Task OnAny<TEx1, TEx2, TEx3>(Action action, params TimeSpan[] delays)
+        public static Task OnAny<TEx1, TEx2, TEx3>(Func<Task> func, params TimeSpan[] delays)
             where TEx1 : Exception where TEx2 : Exception where TEx3 : Exception => 
-            OnImpl(action, delays, typeof(TEx1), typeof(TEx2), typeof(TEx3));
+                OnImpl(func, delays, typeof(TEx1), typeof(TEx2), typeof(TEx3));
 
         /// <summary>
-        /// Retries the given <paramref name="action"/> in case of any of the given exceptions specified by
+        /// Retries the given <paramref name="func"/> in case of any of the given exceptions specified by
         /// <typeparamref name="TEx1"/>, <typeparamref name="TEx2"/>, <typeparamref name="TEx3"/> and <typeparamref name="TEx4"/>.
         /// <remarks>
         /// If the given <paramref name="delays"/> is not supplied then the given 
-        /// <paramref name="action"/> will be retried once.
+        /// <paramref name="func"/> will be retried once.
         /// </remarks>
         /// </summary>
-        public static Task OnAny<TEx1, TEx2, TEx3, TEx4>(Action action, params TimeSpan[] delays)
+        public static Task OnAny<TEx1, TEx2, TEx3, TEx4>(Func<Task> func, params TimeSpan[] delays)
             where TEx1 : Exception where TEx2 : Exception where TEx3 : Exception where TEx4 : Exception =>
-            OnImpl(action, delays, typeof(TEx1), typeof(TEx2), typeof(TEx3), typeof(TEx4));
+                OnImpl(func, delays, typeof(TEx1), typeof(TEx2), typeof(TEx3), typeof(TEx4));
 
         /// <summary>
-        /// Retries the given <paramref name="action"/> in case of any of the given exceptions specified by
+        /// Retries the given <paramref name="func"/> in case of any of the given exceptions specified by
         /// <typeparamref name="TEx1"/>, <typeparamref name="TEx2"/>, <typeparamref name="TEx3"/>, <typeparamref name="TEx4"/> and <typeparamref name="TEx5"/>.
         /// <remarks>
         /// If the given <paramref name="delays"/> is not supplied then the given 
-        /// <paramref name="action"/> will be retried once.
+        /// <paramref name="func"/> will be retried once.
         /// </remarks>
         /// </summary>
-        public static Task OnAny<TEx1, TEx2, TEx3, TEx4, TEx5>(Action action, params TimeSpan[] delays)
+        public static Task OnAny<TEx1, TEx2, TEx3, TEx4, TEx5>(Func<Task> func, params TimeSpan[] delays)
             where TEx1 : Exception where TEx2 : Exception where TEx3 : Exception where TEx4 : Exception where TEx5 : Exception =>
-            OnImpl(action, delays, typeof(TEx1), typeof(TEx2), typeof(TEx3), typeof(TEx4), typeof(TEx5));
+                OnImpl(func, delays, typeof(TEx1), typeof(TEx2), typeof(TEx3), typeof(TEx4), typeof(TEx5));
 
         /// <summary>
         /// Retries the given <paramref name="func"/> in case of an exception of 
@@ -77,7 +77,7 @@ namespace Easy.Common
         /// <paramref name="func"/> will be retried once.
         /// </remarks>
         /// </summary>
-        public static Task<TResult> On<TEx, TResult>(Func<TResult> func, params TimeSpan[] delays) 
+        public static Task<TResult> On<TEx, TResult>(Func<Task<TResult>> func, params TimeSpan[] delays) 
             where TEx : Exception => OnImpl(func, delays, typeof(TEx));
 
         /// <summary>
@@ -88,7 +88,7 @@ namespace Easy.Common
         /// <paramref name="func"/> will be retried once.
         /// </remarks>
         /// </summary>
-        public static Task<TResult> OnAny<TEx1, TEx2, TResult>(Func<TResult> func, params TimeSpan[] delays)
+        public static Task<TResult> OnAny<TEx1, TEx2, TResult>(Func<Task<TResult>> func, params TimeSpan[] delays)
             where TEx1 : Exception where TEx2 : Exception => OnImpl(func, delays, typeof(TEx1), typeof(TEx2));
 
         /// <summary>
@@ -99,9 +99,9 @@ namespace Easy.Common
         /// <paramref name="func"/> will be retried once.
         /// </remarks>
         /// </summary>
-        public static Task<TResult> OnAny<TEx1, TEx2, TEx3, TResult>(Func<TResult> func, params TimeSpan[] delays)
+        public static Task<TResult> OnAny<TEx1, TEx2, TEx3, TResult>(Func<Task<TResult>> func, params TimeSpan[] delays)
             where TEx1 : Exception where TEx2 : Exception where TEx3 : Exception =>
-            OnImpl(func, delays, typeof(TEx1), typeof(TEx2), typeof(TEx3));
+                OnImpl(func, delays, typeof(TEx1), typeof(TEx2), typeof(TEx3));
 
         /// <summary>
         /// Retries the given <paramref name="func"/> in case of any of the given exceptions specified by 
@@ -111,9 +111,9 @@ namespace Easy.Common
         /// <paramref name="func"/> will be retried once.
         /// </remarks>
         /// </summary>
-        public static Task<TResult> OnAny<TEx1, TEx2, TEx3, TEx4, TResult>(Func<TResult> func, params TimeSpan[] delays)
+        public static Task<TResult> OnAny<TEx1, TEx2, TEx3, TEx4, TResult>(Func<Task<TResult>> func, params TimeSpan[] delays)
             where TEx1 : Exception where TEx2 : Exception where TEx3 : Exception where TEx4 : Exception =>
-            OnImpl(func, delays, typeof(TEx1), typeof(TEx2), typeof(TEx3), typeof(TEx4));
+                OnImpl(func, delays, typeof(TEx1), typeof(TEx2), typeof(TEx3), typeof(TEx4));
 
         /// <summary>
         /// Retries the given <paramref name="func"/> in case of any of the given exceptions specified by 
@@ -123,11 +123,11 @@ namespace Easy.Common
         /// <paramref name="func"/> will be retried once.
         /// </remarks>
         /// </summary>
-        public static Task<TResult> OnAny<TEx1, TEx2, TEx3, TEx4, TEx5, TResult>(Func<TResult> func, params TimeSpan[] delays)
+        public static Task<TResult> OnAny<TEx1, TEx2, TEx3, TEx4, TEx5, TResult>(Func<Task<TResult>> func, params TimeSpan[] delays)
             where TEx1 : Exception where TEx2 : Exception where TEx3 : Exception where TEx4 : Exception where TEx5 : Exception =>
-            OnImpl(func, delays, typeof(TEx1), typeof(TEx2), typeof(TEx3), typeof(TEx4), typeof(TEx5));
+                OnImpl(func, delays, typeof(TEx1), typeof(TEx2), typeof(TEx3), typeof(TEx4), typeof(TEx5));
 
-        private static async Task OnImpl(Action action, IReadOnlyList<TimeSpan> delays, params Type[] exceptionTypes)
+        private static async Task OnImpl(Func<Task> func, IReadOnlyList<TimeSpan> delays, params Type[] exceptionTypes)
         {
             if (!delays.Any())
             {
@@ -136,7 +136,7 @@ namespace Easy.Common
                 {
                     try
                     {
-                        action();
+                        await func().ConfigureAwait(false);
                         return;
                     } catch (Exception e) when (exceptionTypes.Contains(e.GetType()))
                     {
@@ -149,7 +149,7 @@ namespace Easy.Common
             {
                 try
                 {
-                    action();
+                    await func().ConfigureAwait(false);
                     return;
                 } catch (Exception e) when (exceptionTypes.Contains(e.GetType()))
                 {
@@ -159,7 +159,7 @@ namespace Easy.Common
             }
         }
 
-        private static async Task<TResult> OnImpl<TResult>(Func<TResult> func, IReadOnlyList<TimeSpan> delays, params Type[] exceptionTypes)
+        private static async Task<TResult> OnImpl<TResult>(Func<Task<TResult>> func, IReadOnlyList<TimeSpan> delays, params Type[] exceptionTypes)
         {
             if (!delays.Any())
             {
@@ -168,7 +168,7 @@ namespace Easy.Common
                 {
                     try
                     {
-                        return func();
+                        return await func().ConfigureAwait(false);
                     } catch (Exception e) when (exceptionTypes.Contains(e.GetType()))
                     {
                         if (count == 0) { throw new RetryException(1, e); }
@@ -180,7 +180,7 @@ namespace Easy.Common
             {
                 try
                 {
-                    return func();
+                    return await func().ConfigureAwait(false);
                 } catch (Exception e) when (exceptionTypes.Contains(e.GetType()))
                 {
                     if (i == delays.Count) { throw new RetryException((uint)i, e); }
