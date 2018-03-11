8a7c067c762239a91882c41a4c08953d22c5b771
Nima <Nima.Ara@outlook.com>
2018-03-11 18:29:10 +0000

    Added DetectEncoding extension method for Streams.

---
+++ Easy.Common.Tests.Unit/StreamExtensions/DetectingEncodingTests.cs
@@ -0,0 +1,76 @@
+﻿namespace Easy.Common.Tests.Unit.StreamExtensions
+{
+    using System.IO;
+    using System.Text;
+    using Easy.Common.Extensions;
+    using NUnit.Framework;
+    using Shouldly;
+
+    [TestFixture]
+    internal sealed class DetectingEncodingTests
+    {
+        [Test]
+        public void When_detecting_empty_stream()
+        {
+            var defaultEncodingIfNoBom = Encoding.ASCII;
+            
+            using (var mem = new MemoryStream())
+            {
+                mem.Position = 0;
+                mem.DetectEncoding(defaultEncodingIfNoBom).ShouldBe(Encoding.ASCII);
+                mem.Position.ShouldBe(0);
+            }
+        }
+
+        [Test]
+        public void When_detecting_utf8()
+        {
+            var defaultEncodingIfNoBom = Encoding.ASCII;
+            
+            using (var mem = new MemoryStream())
+            using (var writer = new StreamWriter(mem, Encoding.UTF8))
+            {
+                writer.Write("Hello");
+                writer.Flush();
+
+                mem.Position = 0;
+                mem.DetectEncoding(defaultEncodingIfNoBom).ShouldBe(Encoding.UTF8);
+                mem.Position.ShouldBe(0);
+            }
+        }
+
+        [Test]
+        public void When_detecting_utf16()
+        {
+            var defaultEncodingIfNoBom = Encoding.ASCII;
+            
+            using (var mem = new MemoryStream())
+            using (var writer = new StreamWriter(mem, Encoding.Unicode))
+            {
+                writer.Write("Hello");
+                writer.Flush();
+
+                mem.Position = 0;
+                mem.DetectEncoding(defaultEncodingIfNoBom).ShouldBe(Encoding.Unicode);
+                mem.Position.ShouldBe(0);
+            }
+        }
+
+        [Test]
+        public void When_detecting_utf32()
+        {
+            var defaultEncodingIfNoBom = Encoding.ASCII;
+            
+            using (var mem = new MemoryStream())
+            using (var writer = new StreamWriter(mem, Encoding.UTF32))
+            {
+                writer.Write("Hello");
+                writer.Flush();
+
+                mem.Position = 0;
+                mem.DetectEncoding(defaultEncodingIfNoBom).ShouldBe(Encoding.UTF32);
+                mem.Position.ShouldBe(0);
+            }
+        }
+    }
+}
--- Easy.Common/Extensions/StreamExtensions.cs
+++ Easy.Common/Extensions/StreamExtensions.cs
@@ -2,6 +2,7 @@
 {
     using System.Collections.Generic;
     using System.IO;
+    using System.Text;
     using System.Xml;
     using System.Xml.Linq;
 
@@ -11,13 +12,37 @@
     public static class StreamExtensions
     {
         /// <summary>
+        /// Detects the text encoding for the given <paramref name="stream"/>.
+        /// </summary>
+        public static Encoding DetectEncoding(this Stream stream, Encoding defaultEncodingIfNoBOM)
+        {
+            var startPos = stream.Position;
+
+            try
+            {
+                using (var reader = new StreamReader(stream, defaultEncodingIfNoBOM, true, 256, true))
+                {
+                    var _ = reader.Peek();
+                    return reader.CurrentEncoding;
+                }
+            } finally
+            {
+                if (stream.CanSeek)
+                {
+                    stream.Position = startPos;
+                }
+            }
+        }
+        
+        /// <summary>
         /// Gets a sequence containing every element with the name equal to <paramref name="name"/>.
         /// </summary>
         /// <param name="stream">The stream containing XML</param>
         /// <param name="name">The name of the elements to return</param>
         /// <param name="ignoreCase">The flag indicating whether the name should be looked up in a case sensitive manner</param>
         /// <returns>The sequence containing all the elements <see cref="XElement"/> matching the <paramref name="name"/></returns>
-        public static IEnumerable<XElement> GetElements(this Stream stream, XName name, bool ignoreCase = true)
+        public static IEnumerable<XElement> GetElements(
+            this Stream stream, XName name, bool ignoreCase = true)
         {
             Ensure.NotNull(stream, nameof(stream));
             return stream.GetElements(name, new XmlReaderSettings(), ignoreCase);
@@ -31,7 +56,8 @@
         /// <param name="settings">The settings used by the <see cref="XmlReader"/></param>
         /// <param name="ignoreCase">The flag indicating whether the name should be looked up in a case sensitive manner</param>
         /// <returns>The sequence containing all the elements <see cref="XElement"/> matching the <paramref name="name"/></returns>
-        public static IEnumerable<XElement> GetElements(this Stream stream, XName name, XmlReaderSettings settings, bool ignoreCase = true)
+        public static IEnumerable<XElement> GetElements(
+            this Stream stream, XName name, XmlReaderSettings settings, bool ignoreCase = true)
         {
             Ensure.NotNull(stream, nameof(stream));
             Ensure.NotNull(name, nameof(name));
@@ -46,5 +72,4 @@
             }
         }
     }
-
 }
