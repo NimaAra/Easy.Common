ff6f76c36c7538765cfdbddbf13785440bfa4d43
Nima <Nima.Ara@outlook.com>
2018-03-12 21:52:05 +0000

    Added ReadLines and CountLines extenion methods for Streams.

---
+++ Easy.Common.Tests.Unit/StreamExtensions/CountingLinesTests.cs
@@ -0,0 +1,153 @@
+﻿namespace Easy.Common.Tests.Unit.StreamExtensions
+{
+    using System;
+    using System.IO;
+    using System.Text;
+    using Easy.Common.Extensions;
+    using NUnit.Framework;
+    using Shouldly;
+
+    [TestFixture]
+    internal sealed class CountingLinesTests
+    {
+        [Test]
+        public void When_reading_null_stream()
+        {
+            Should.Throw<ArgumentNullException>(() => ((MemoryStream) null).CountLines())
+                .Message.ShouldBe("Value cannot be null.\r\nParameter name: stream");
+        }
+        
+        [Test]
+        public void When_reading_empty_stream()
+        {
+            using (var mem = new MemoryStream())
+            {
+                mem.CountLines().ShouldBe(0);
+            }
+        }
+        
+        [TestCase("\r")]
+        [TestCase("\n")]
+        [TestCase("\r\n")]
+        public void When_reading_stream_with_empty_line(string content)
+        {
+            using (var mem = new MemoryStream())
+            {
+                var input = Encoding.UTF8.GetBytes(content);
+                mem.Write(input, 0, input.Length);
+                mem.Position = 0;
+
+                mem.CountLines().ShouldBe(1);
+            }
+        }
+
+        [TestCase("\r\r")]
+        [TestCase("\n\n")]
+        [TestCase("\r\n\r\n")]
+        public void When_reading_stream_with_empty_lines(string content)
+        {
+            using (var mem = new MemoryStream())
+            {
+                var input = Encoding.UTF8.GetBytes(content);
+                mem.Write(input, 0, input.Length);
+                mem.Position = 0;
+
+                mem.CountLines().ShouldBe(2);
+            }
+        }
+
+        [TestCase("A")]
+        [TestCase("A\r")]
+        [TestCase("A\n")]
+        [TestCase("A\r\n")]
+        public void When_reading_stream_with_single_line(string content)
+        {
+            using (var mem = new MemoryStream())
+            {
+                var input = Encoding.UTF8.GetBytes(content);
+                mem.Write(input, 0, input.Length);
+                mem.Position = 0;
+
+                mem.CountLines().ShouldBe(1);
+            }
+        }
+
+        [Test]
+        public void When_reading_stream_with_lines_terminated_by_carriage_return()
+        {
+            using (var mem = new MemoryStream())
+            {
+                var input = Encoding.UTF8.GetBytes("A\rB\r\r");
+                mem.Write(input, 0, input.Length);
+                mem.Position = 0;
+
+                mem.CountLines().ShouldBe(3);
+            }
+        }
+
+        [Test]
+        public void When_reading_stream_with_lines_terminated_by_new_line()
+        {
+            using (var mem = new MemoryStream())
+            {
+                var input = Encoding.UTF8.GetBytes("A\nB\n\n");
+                mem.Write(input, 0, input.Length);
+                mem.Position = 0;
+
+                mem.CountLines().ShouldBe(3);
+            }
+        }
+
+        [Test]
+        public void When_reading_stream_with_lines_terminated_by_carriage_return_followed_by_new_line()
+        {
+            using (var mem = new MemoryStream())
+            {
+                var input = Encoding.UTF8.GetBytes("A\r\nB\r\n\r\n");
+                mem.Write(input, 0, input.Length);
+                mem.Position = 0;
+
+                mem.CountLines().ShouldBe(3);
+            }
+        }
+
+        [Test]
+        public void When_reading_long_line_terminated_by_carriage_return()
+        {
+            using (var mem = new MemoryStream())
+            {
+                var input = Encoding.UTF8.GetBytes($"A\r{new string('_', 1_500_000)}\rB");
+                mem.Write(input, 0, input.Length);
+                mem.Position = 0;
+
+                mem.CountLines().ShouldBe(3);
+            }
+        }
+
+        [Test]
+        public void When_reading_long_line_terminated_by_carriage_by_new_line()
+        {
+            using (var mem = new MemoryStream())
+            {
+                var input = Encoding.UTF8.GetBytes($"A\n{new string('_', 1_500_000)}\nB");
+                mem.Write(input, 0, input.Length);
+                mem.Position = 0;
+
+                mem.CountLines().ShouldBe(3);
+            }
+        }
+
+        [Test]
+        public void When_reading_long_line_terminated_by_carriage_return_followed_by_new_line()
+        {
+            using (var mem = new MemoryStream())
+            {
+                var input = Encoding.UTF8.GetBytes($"A\r\n{new string('_', 1_500_000)}\r\nB");
+                mem.Write(input, 0, input.Length);
+                mem.Position = 0;
+
+                mem.CountLines().ShouldBe(3);
+            }
+        }
+    }
+}
--- Easy.Common/Extensions/StreamExtensions.cs
+++ Easy.Common/Extensions/StreamExtensions.cs
@@ -1,6 +1,8 @@
 ﻿namespace Easy.Common.Extensions
 {
+    using System;
     using System.Collections.Generic;
+    using System.Diagnostics;
     using System.IO;
     using System.Text;
     using System.Xml;
@@ -11,11 +13,17 @@
     /// </summary>
     public static class StreamExtensions
     {
+        private const char CR = '\r';
+        private const char NL = '\n';
+        
         /// <summary>
         /// Detects the text encoding for the given <paramref name="stream"/>.
         /// </summary>
+        [DebuggerStepThrough]
         public static Encoding DetectEncoding(this Stream stream, Encoding defaultEncodingIfNoBOM)
         {
+            Ensure.NotNull(stream, nameof(stream));
+            
             var startPos = stream.Position;
 
             try
@@ -27,13 +35,120 @@
                 }
             } finally
             {
-                if (stream.CanSeek)
-                {
-                    stream.Position = startPos;
-                }
+                if (stream.CanSeek) { stream.Position = startPos; }
             }
         }
         
+        /// <summary>
+        /// Returns a sequence of lines read from the given <paramref name="stream"/>.
+        /// </summary>
+        [DebuggerStepThrough]
+        public static IEnumerable<string> ReadLines(this Stream stream) 
+            => stream.ReadLines(stream.DetectEncoding(Encoding.UTF8));
+
+        /// <summary>
+        /// Returns a sequence of lines read from the given <paramref name="stream"/>.
+        /// </summary>
+        [DebuggerStepThrough]
+        public static IEnumerable<string> ReadLines(this Stream stream, Encoding encoding)
+        {
+            Ensure.NotNull(stream, nameof(stream));
+
+            var decoder = encoding.GetDecoder();
+
+            var byteBuffer = new byte[1024 * 1024];
+            var charBuffer = new char[byteBuffer.Length];
+            var lineChars = new char[4096];
+            var lineCharsIdx = 0;
+
+            var prevChar = '\0';
+
+            int bytesRead;
+            while ((bytesRead = stream.Read(byteBuffer, 0, byteBuffer.Length)) > 0)
+            {
+                var charsConverted = decoder.GetChars(byteBuffer, 0, bytesRead, charBuffer, 0);
+
+                for (var i = 0; i < charsConverted; i++)
+                {
+                    var currentChar = charBuffer[i];
+                    
+                    if (currentChar == CR || currentChar == NL)
+                    {
+                        if (prevChar == CR && currentChar == NL) { continue; }
+
+                        yield return new string(lineChars, 0, lineCharsIdx);
+                        lineCharsIdx = 0;
+                    } else
+                    {
+                        if (lineCharsIdx == lineChars.Length)
+                        {
+                            Array.Resize(ref lineChars, lineChars.Length * 2);
+                        }
+                        lineChars[lineCharsIdx++] = currentChar;
+                    }
+
+                    prevChar = currentChar;
+                }
+            }
+
+            if (lineCharsIdx > 0)
+            {
+                yield return new string(lineChars, 0, lineCharsIdx);
+            }
+        }
+
+        /// <summary>
+        /// Returns the number of lines in the given <paramref name="stream"/>.
+        /// </summary>
+        [DebuggerStepThrough]
+        public static long CountLines(this Stream stream, bool leaveOpen = false) 
+            => stream.CountLines(stream.DetectEncoding(Encoding.UTF8));
+
+        /// <summary>
+        /// Returns the number of lines in the given <paramref name="stream"/>.
+        /// </summary>
+        [DebuggerStepThrough]
+        public static long CountLines(this Stream stream, Encoding encoding)
+        {
+            Ensure.NotNull(stream, nameof(stream));
+
+            var lineCount = 0L;
+
+            var decoder = encoding.GetDecoder();
+
+            var byteBuffer = new byte[1024 * 1024];
+            var charBuffer = new char[byteBuffer.Length];
+            var lineTerminated = false;
+            var prevChar = '\0';
+
+            int bytesRead;
+            while ((bytesRead = stream.Read(byteBuffer, 0, byteBuffer.Length)) > 0)
+            {
+                var charsConverted = decoder.GetChars(byteBuffer, 0, bytesRead, charBuffer, 0);
+
+                for (var i = 0; i < charsConverted; i++)
+                {
+                    var currentChar = charBuffer[i];
+                    
+                    if (currentChar == CR || currentChar == NL)
+                    {
+                        if (prevChar == CR && currentChar == NL) { continue; }
+
+                        lineCount++;
+                        lineTerminated = false;
+                    } else
+                    {
+                        lineTerminated = true;
+                    }
+
+                    prevChar = currentChar;
+                }
+            }
+
+            if (lineTerminated) { lineCount++; }
+            return lineCount;
+        }
+
         /// <summary>
         /// Gets a sequence containing every element with the name equal to <paramref name="name"/>.
         /// </summary>
@@ -41,6 +156,7 @@
         /// <param name="name">The name of the elements to return</param>
         /// <param name="ignoreCase">The flag indicating whether the name should be looked up in a case sensitive manner</param>
         /// <returns>The sequence containing all the elements <see cref="XElement"/> matching the <paramref name="name"/></returns>
+        [DebuggerStepThrough]
         public static IEnumerable<XElement> GetElements(
             this Stream stream, XName name, bool ignoreCase = true)
         {
@@ -56,6 +172,7 @@
         /// <param name="settings">The settings used by the <see cref="XmlReader"/></param>
         /// <param name="ignoreCase">The flag indicating whether the name should be looked up in a case sensitive manner</param>
         /// <returns>The sequence containing all the elements <see cref="XElement"/> matching the <paramref name="name"/></returns>
+        [DebuggerStepThrough]
         public static IEnumerable<XElement> GetElements(
             this Stream stream, XName name, XmlReaderSettings settings, bool ignoreCase = true)
         {
