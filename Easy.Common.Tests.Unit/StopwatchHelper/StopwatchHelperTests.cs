f5517bf989b1b1ac93d68a7b844c9562d0bad196
Nima Ara <NimaAra@users.noreply.github.com>
2018-10-14 20:59:03 +0100

    Added 'StopwatchHelper`.

---
+++ Easy.Common.Tests.Unit/StopwatchHelper/StopwatchHelperTests.cs
@@ -0,0 +1,96 @@
+﻿namespace Easy.Common.Tests.Unit.StopwatchHelper
+{
+    using System.Diagnostics;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+    using Shouldly;
+    using StopwatchHelper = Easy.Common.StopwatchHelper;
+
+    [TestFixture]
+    internal sealed class StopwatchHelperTests
+    {
+        [Test]
+        public async Task When_getting_duration_in_milliseconds_since()
+        {
+            const int DELAY_DURATION = 150;
+            
+            var start = Stopwatch.GetTimestamp();
+
+            await Task.Delay(150).ConfigureAwait(false);
+
+            var duration = StopwatchHelper.GetDurationInMillisecondsSince(start);
+            duration.ShouldBeGreaterThanOrEqualTo(DELAY_DURATION);
+        }
+
+        [Test]
+        public async Task When_getting_duration_in_seconds_since()
+        {
+            const int DELAY_DURATION = 1;
+            
+            var start = Stopwatch.GetTimestamp();
+
+            await Task.Delay(DELAY_DURATION * 1000).ConfigureAwait(false);
+
+            var duration = StopwatchHelper.GetDurationInSecondsSince(start);
+            duration.ShouldBeGreaterThanOrEqualTo(DELAY_DURATION);
+        }
+
+        [Test]
+        public async Task When_getting_duration_since()
+        {
+            const int DELAY_DURATION = 200;
+            
+            var start = Stopwatch.GetTimestamp();
+
+            await Task.Delay(DELAY_DURATION).ConfigureAwait(false);
+
+            var duration = StopwatchHelper.GetDurationSince(start);
+            duration.TotalMilliseconds.ShouldBeGreaterThanOrEqualTo(DELAY_DURATION);
+        }
+
+        [Test]
+        public async Task When_getting_difference_in_milliseconds()
+        {
+            const int DELAY_DURATION = 150;
+            
+            var start = Stopwatch.GetTimestamp();
+
+            await Task.Delay(150).ConfigureAwait(false);
+
+            var end = Stopwatch.GetTimestamp();
+
+            var duration = StopwatchHelper.GetDurationInMilliseconds(start, end);
+            duration.ShouldBeGreaterThanOrEqualTo(DELAY_DURATION);
+        }
+
+        [Test]
+        public async Task When_getting_difference_in_seconds()
+        {
+            const int DELAY_DURATION = 1;
+            
+            var start = Stopwatch.GetTimestamp();
+
+            await Task.Delay(DELAY_DURATION * 1000).ConfigureAwait(false);
+
+            var end = Stopwatch.GetTimestamp();
+
+            var duration = StopwatchHelper.GetDurationInSeconds(start, end);
+            duration.ShouldBeGreaterThanOrEqualTo(DELAY_DURATION);
+        }
+
+        [Test]
+        public async Task When_getting_difference()
+        {
+            const int DELAY_DURATION = 200;
+            
+            var start = Stopwatch.GetTimestamp();
+
+            await Task.Delay(DELAY_DURATION).ConfigureAwait(false);
+
+            var end = Stopwatch.GetTimestamp();
+
+            var duration = StopwatchHelper.GetDuration(start, end);
+            duration.TotalMilliseconds.ShouldBeGreaterThanOrEqualTo(DELAY_DURATION);
+        }
+    }
+}
---
+++ Easy.Common/StopwatchHelper.cs
@@ -0,0 +1,47 @@
+﻿namespace Easy.Common
+{
+    using System;
+    using System.Diagnostics;
+
+    /// <summary>
+    /// Provides a set of methods to help work with <see cref="Stopwatch"/>.
+    /// </summary>
+    public static class StopwatchHelper
+    {
+        /// <summary>
+        /// Returns the duration between now and the given <paramref name="startTime"/> in milliseconds.
+        /// </summary>
+        public static double GetDurationInMillisecondsSince(long startTime) =>
+            GetDurationInMilliseconds(startTime, Stopwatch.GetTimestamp());
+
+        /// <summary>
+        /// Returns the duration between now and the given <paramref name="startTime"/> in seconds.
+        /// </summary>
+        public static double GetDurationInSecondsSince(long startTime) =>
+            GetDurationInSeconds(startTime, Stopwatch.GetTimestamp());
+
+        /// <summary>
+        /// Returns the duration between now and the given <paramref name="startTime"/>.
+        /// </summary>
+        public static TimeSpan GetDurationSince(long startTime) =>
+            GetDuration(startTime, Stopwatch.GetTimestamp());
+
+        /// <summary>
+        /// Returns the duration between <paramref name="from"/> and <paramref name="to"/>.
+        /// </summary>
+        public static TimeSpan GetDuration(long from, long to) => 
+            TimeSpan.FromMilliseconds(GetDurationInMilliseconds(from, to));
+
+        /// <summary>
+        /// Returns the duration between <paramref name="from"/> and <paramref name="to"/> in milliseconds.
+        /// </summary>
+        public static double GetDurationInMilliseconds(long from, long to) => 
+            GetDurationInSeconds(from, to) * 1000;
+
+        /// <summary>
+        /// Returns the duration between <paramref name="from"/> and <paramref name="to"/> in seconds.
+        /// </summary>
+        public static double GetDurationInSeconds(long from, long to) => 
+            ((double)to - from) / Stopwatch.Frequency;
+    }
+}
