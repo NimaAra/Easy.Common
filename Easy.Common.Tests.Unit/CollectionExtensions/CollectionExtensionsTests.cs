d915ef11b35d5ce97b78fec921ce0cfbc21cde41
Nima Ara <NimaAra@users.noreply.github.com>
2019-02-26 22:10:41 +0000

    Added CollectionExtensions.

---
+++ Easy.Common.Tests.Unit/CollectionExtensions/CollectionExtensionsTests.cs
@@ -0,0 +1,85 @@
+﻿namespace Easy.Common.Tests.Unit.CollectionExtensions
+{
+    using System;
+    using System.Collections.Generic;
+    using Easy.Common.Extensions;
+    using NUnit.Framework;
+    using Shouldly;
+
+    [TestFixture]
+    internal sealed class CollectionExtensionsTests
+    {
+        [Test]
+        public void When_generating_a_tree()
+        {
+            var someData = new[]
+            {
+                new Category(1, 0, "Sport"),
+                new Category(2, 1, "Balls"),
+                new Category(3, 1, "Shoes"),
+                new Category(4, 0, "Electronics"),
+                new Category(5, 4, "Cameras"),
+                new Category(6, 5, "Lenses"),  
+                new Category(7, 5, "Tripod"), 
+                new Category(8, 4, "Computers"),
+                new Category(9, 8, "Laptops"),
+                new Category(10, 0, "Empty"),
+                new Category(11, 12, "Invalid")
+            };
+
+            var result = someData.ToTree(x => x.Id, x => x.ParentId);
+
+            PrintTree(result);
+
+            result.Length.ShouldBe(3);
+            
+            result[0].Value.Name.ShouldBe("Sport");
+            result[0].Children.Length.ShouldBe(2);
+                result[0].Children[0].Value.Name.ShouldBe("Balls");
+                result[0].Children[0].Children.ShouldBeEmpty();
+                result[0].Children[1].Value.Name.ShouldBe("Shoes");
+                result[0].Children[1].Children.ShouldBeEmpty();
+
+            result[1].Value.Name.ShouldBe("Electronics");
+            result[1].Children.Length.ShouldBe(2);
+                result[1].Children[0].Value.Name.ShouldBe("Cameras");
+                result[1].Children[0].Children.Length.ShouldBe(2);
+                    result[1].Children[0].Children[0].Value.Name.ShouldBe("Lenses");
+                    result[1].Children[0].Children[0].Children.ShouldBeEmpty();
+                    result[1].Children[0].Children[1].Value.Name.ShouldBe("Tripod");
+                    result[1].Children[0].Children[1].Children.ShouldBeEmpty();
+                result[1].Children[1].Value.Name.ShouldBe("Computers");
+                result[1].Children[1].Children.Length.ShouldBe(1);
+                    result[1].Children[1].Children[0].Value.Name.ShouldBe("Laptops");
+                    result[1].Children[1].Children[0].Children.ShouldBeEmpty();
+
+            result[2].Value.Name.ShouldBe("Empty");
+            result[2].Children.ShouldBeEmpty();
+            
+            void PrintTree(IEnumerable<TreeNode<Category>> tree, int indent = 0)
+            {
+                foreach (var c in tree)
+                {
+                    Console.WriteLine(new string('\t', indent) + c.Value.Name);
+                    PrintTree(c.Children, indent + 1);
+                }
+            }
+        }
+
+        private sealed class Category
+        {
+            public int Id { get; }
+            public int ParentId { get; }
+            public string Name { get; }
+
+            public Category(int id, int parentId, string name)
+            {
+                Id = id;
+                ParentId = parentId;
+                Name = name;
+            }
+
+            public override string ToString() => $"Id: {Id} - ParentId: {ParentId} - Name: {Name}";
+        }
+    }
+}
---
+++ Easy.Common/Extensions/CollectionExtensions.cs
@@ -0,0 +1,54 @@
+﻿namespace Easy.Common.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// Extension methods for <see cref="ICollection{T}"/>
+    /// </summary>
+    public static class CollectionExtensions
+    {
+        /// <summary>
+        /// Generates an array of <see cref="TreeNode{T}"/> from the given <paramref name="collection"/>
+        /// based on parent-child relationship.
+        /// </summary>
+        /// <typeparam name="T">The type of each value in the node.</typeparam>
+        /// <typeparam name="TId">The type of the key used for determining parent-child relationship.</typeparam>
+        public static TreeNode<T>[] ToTree<T, TId>(
+            this ICollection<T> collection,
+            Func<T, TId> idSelector,
+            Func<T, TId> parentIdSelector,
+            TId rootId = default(TId)) => 
+            collection
+                .Where(x => EqualityComparer<TId>.Default.Equals(parentIdSelector(x), rootId))
+                .Select(x => new TreeNode<T>(x, collection.ToTree(idSelector, parentIdSelector, idSelector(x))))
+                .ToArray();
+    }
+
+    /// <summary>
+    /// Represents a node of a tree.
+    /// </summary>
+    /// <typeparam name="T">The type of the value in each node.</typeparam>
+    public sealed class TreeNode<T>
+    {
+        /// <summary>
+        /// Gets the value of the node.
+        /// </summary>
+        public T Value { get; }
+
+        /// <summary>
+        /// Gets the children of this node.
+        /// </summary>
+        public TreeNode<T>[] Children { get; }
+
+        /// <summary>
+        /// Creates an instance of the <see cref="TreeNode{T}"/>.
+        /// </summary>
+        public TreeNode(T value, TreeNode<T>[] children)
+        {
+            Value = value;
+            Children = children;
+        }
+    }
+}
