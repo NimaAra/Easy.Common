3b1c594e14eed2605e861962e75ad4105e24e80e
Nima Ara <NimaAra@users.noreply.github.com>
2019-04-07 14:02:39 +0100

    Added the feature to support asynchronously waiting for a value of Lazy<Task<T>>.

---
+++ Easy.Common.Tests.Unit/LazyExtensions/LazyExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿namespace Easy.Common.Tests.Unit.LazyExtensions
+{
+    using System;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+    using Shouldly;
+    using Easy.Common.Extensions;
+
+    [TestFixture]
+    internal sealed class LazyExtensionsTests
+    {
+        [Test]
+        public async Task When_asynchronously_waiting_for_the_result()
+        {
+            var lazy = new Lazy<Task<int>>(() => Task.FromResult(42));
+            var result = await lazy;
+            result.ShouldBe(42);
+            lazy.Value.Result.ShouldBe(42);
+        }
+    }
+}
