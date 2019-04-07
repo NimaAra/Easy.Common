97304542f4df0249dd40dc75c8801700b7602fb9
Nima Ara <NimaAra@users.noreply.github.com>
2019-04-07 17:12:54 +0100

    Added the feature to support initializing an IList with multiple items.

---
+++ Easy.Common.Tests.Unit/ListExtensions/ListExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿namespace Easy.Common.Tests.Unit.ListExtensions
+{
+    using System.Collections.Generic;
+    using NUnit.Framework;
+    using Shouldly;
+    using Easy.Common.Extensions;
+
+    [TestFixture]
+    internal sealed class ListExtensionsTests
+    {
+        [Test]
+        public void When_initializing_a_list_with_multiple_items()
+        {
+            var list = new List<int>
+            {
+                1,
+                new[] {2, 3, 4, 5},
+                6
+            };
+
+            list.Count.ShouldBe(6);
+            list.ShouldBe(new [] {1, 2, 3, 4, 5, 6});
+        }
+    }
+}
---
+++ Easy.Common/Extensions/ListExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace Easy.Common.Extensions
+{
+    using System.Collections.Generic;
+    using System.Diagnostics;
+
+    /// <summary>
+    /// Extension methods for <see cref="IList{T}"/>
+    /// </summary>
+    public static class ListExtensions
+    {
+        /// <summary>
+        /// Adds the given <paramref name="items"/> to the given <paramref name="list"/>.
+        /// <remarks>This method is used to duck-type <see cref="IList{T}"/> with multiple items.</remarks>
+        /// </summary>
+        [DebuggerStepThrough]
+        public static void Add<T>(this IList<T> list, IEnumerable<T> items)
+        {
+            foreach (var item in items)
+            {
+                list.Add(item);
+            }
+        }
+    }
+}
