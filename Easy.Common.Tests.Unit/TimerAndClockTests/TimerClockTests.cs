92f55a0ba2d9f80d86490d626ac511c1cbec8fb3
Nima Ara <NimaAra@users.noreply.github.com>
2020-04-28 16:36:31 +0100

    Improve TimerClock tests

--- Easy.Common.Tests.Unit/TimerAndClockTests/TimerClockTests.cs
+++ Easy.Common.Tests.Unit/TimerAndClockTests/TimerClockTests.cs
@@ -12,78 +12,72 @@
         public void When_scheduling_an_action_to_execute_at_every_interval()
         {
             var counter = 0;
-            
-            using (var clock = new TimerClock(10.Milliseconds()))
-            {
-                clock.Tick += (sender, args) => Interlocked.Increment(ref counter);
 
-                clock.Enabled.ShouldBeFalse();
+            using var clock = new TimerClock(10.Milliseconds());
+            clock.Tick += (sender, args) => Interlocked.Increment(ref counter);
 
-                clock.Enabled = true;
+            clock.Enabled.ShouldBeFalse();
 
-                Thread.Sleep(1.Seconds());
+            clock.Enabled = true;
+
+            Thread.Sleep(1.Seconds());
                 
-                var snapshot = counter;
+            var snapshot = counter;
 
-                snapshot.ShouldBeInRange(50, 150);
+            snapshot.ShouldBeInRange(10, 200);
 
-                clock.Enabled.ShouldBeTrue();
-                clock.Enabled = false;
-                clock.Enabled.ShouldBeFalse();
-            }
+            clock.Enabled.ShouldBeTrue();
+            clock.Enabled = false;
+            clock.Enabled.ShouldBeFalse();
         }
 
         [Test]
         public void When_scheduling_multiple_actions_to_execute_at_every_interval()
         {
-            using (var clockA = new TimerClock(500.Milliseconds()))
-            using (var clockB = new TimerClock(100.Milliseconds()))
-            {
-                var counter = 0;
+            using var clockA = new TimerClock(500.Milliseconds());
+            using var clockB = new TimerClock(100.Milliseconds());
+            var counter = 0;
 
-                clockA.Tick += (sender, args) => Interlocked.Increment(ref counter);
-                clockA.Enabled = true;
+            clockA.Tick += (sender, args) => Interlocked.Increment(ref counter);
+            clockA.Enabled = true;
 
-                Thread.Sleep(900.Milliseconds());
+            Thread.Sleep(900.Milliseconds());
 
-                var snapshotA = counter;
+            var snapshotA = counter;
 
-                snapshotA.ShouldBe(1);
+            snapshotA.ShouldBe(1);
 
-                clockB.Tick += (sender, args) => Interlocked.Increment(ref counter);
-                clockB.Enabled = true;
+            clockB.Tick += (sender, args) => Interlocked.Increment(ref counter);
+            clockB.Enabled = true;
 
-                Thread.Sleep(1.Seconds());
+            Thread.Sleep(1.Seconds());
 
-                var snapshotB = counter;
-                snapshotB.ShouldBeInRange(7, 20);
-            }
+            var snapshotB = counter;
+            snapshotB.ShouldBeInRange(7, 20);
         }
 
         [Test]
         public void When_scheduling_an_action_to_execute_after_a_timeout()
         {
-            using (var clock = new TimerClock(300.Milliseconds()))
+            using var clock = new TimerClock(300.Milliseconds());
+            var counter = 0;
+
+            clock.Tick += (sender, args) =>
             {
-                var counter = 0;
+                var clockCopy = sender as TimerClock;
+                // ReSharper disable once PossibleNullReferenceException
+                clockCopy.Enabled = false;
+                counter += 666;
+            };
+            clock.Enabled = true;
 
-                clock.Tick += (sender, args) =>
-                {
-                    var clockCopy = sender as TimerClock;
-                    // ReSharper disable once PossibleNullReferenceException
-                    clockCopy.Enabled = false;
-                    counter += 666;
-                };
-                clock.Enabled = true;
+            Thread.Sleep(100.Milliseconds());
 
-                Thread.Sleep(100.Milliseconds());
+            Assert.AreEqual(0, counter);
 
-                Assert.AreEqual(0, counter);
+            Thread.Sleep(1.Seconds());
 
-                Thread.Sleep(1.Seconds());
-
-                Assert.AreEqual(666, counter);
-            }
+            Assert.AreEqual(666, counter);
         }
     }
 }
