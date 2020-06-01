f9d64bae57ac454aa38a7260b13b4c4cf6a3577e
Piotr Pasieka <ppasieka@gmail.com>
2020-06-01 10:54:41 +0200

    Fix #30

---
+++ Easy.Common.Tests.Unit/BaseEncoding/Base36Tests.cs
@@ -0,0 +1,15 @@
+﻿namespace Easy.Common.Tests.Unit.BaseEncoding
+{
+    using NUnit.Framework;
+    using Shouldly;
+
+    [TestFixture]
+    public class Base36Tests
+    {
+        [TestCase]
+        public void When_encoding() => Base36.Encode(10).ShouldBe("A");
+        
+        [TestCase]
+        public void When_decoding()  => Base36.Decode("A").ShouldBe(10);
+    }
+}
