8cc3125fd43c3f23b88b22945e9697f6955cf911
Nima Ara <Nima.Ara@Outlook.com>
2020-06-01 21:53:55 +0100

    Add tests for Base64 encoding/decoding

---
+++ Easy.Common.Tests.Unit/BaseEncoding/Base64Tests.cs
@@ -0,0 +1,15 @@
+﻿namespace Easy.Common.Tests.Unit.BaseEncoding
+{
+    using NUnit.Framework;
+    using Shouldly;
+
+    [TestFixture]
+    public class Base64Tests
+    {
+        [TestCase]
+        public void When_encoding() => Base64.Encode(new byte[] {1, 15, 30, 40, 0, 13, 10, 43}).ShouldBe("AQ8eKAANCis");
+
+        [TestCase]
+        public void When_decoding() => Base64.Decode("AQ8eKAANCis").ShouldBe(new byte[] { 1, 15, 30, 40, 0, 13, 10, 43 });
+    }
+}
