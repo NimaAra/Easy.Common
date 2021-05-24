c7de99c410534913efbdd0882f38e3b391715c05
Nima Ara <Nima.Ara@Outlook.com>
2021-05-24 13:47:20 +0100

    Add ReadOnlyListExtensions

---
+++ Easy.Common/Extensions/ReadOnlyListExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace Easy.Common.Extensions
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Extension methods for <see cref="IReadOnlyList{T}"/>
+    /// </summary>
+    public static class ReadOnlyListExtensions
+    {
+        /// <summary>
+        /// Searches for the specified <paramref name="element"/> and returns the index of
+        /// its first occurrence in <paramref name="self"/>.
+        /// </summary>
+        public static int IndexOf<T>(this IReadOnlyList<T> self, T element)
+        {
+            EqualityComparer<T> comparer = EqualityComparer<T>.Default;
+            for (var i = 0; i < self.Count; i++)
+            {
+                T item = self[i];
+                if (comparer.Equals(item, element))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
