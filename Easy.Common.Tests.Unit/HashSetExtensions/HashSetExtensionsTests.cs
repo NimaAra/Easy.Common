992ec28d49f39550b252b765eacff5d2419e7e6c
Nima Ara <NimaAra@users.noreply.github.com>
2022-01-25 21:04:45 +0000

    Add AddRange extension method to HashSet<T>

---
+++ Easy.Common.Tests.Unit/HashSetExtensions/HashSetExtensionsTests.cs
@@ -0,0 +1,50 @@
+﻿namespace Easy.Common.Tests.Unit.HashSetExtensions;
+
+using System;
+using System.Collections.Generic;
+using Easy.Common.Extensions;
+using NUnit.Framework;
+using Shouldly;
+
+[TestFixture]
+internal sealed class HashSetExtensionsTests
+{
+    [Test]
+    public void When_adding_multiple_items_to_an_empty_set()
+    {
+        HashSet<int> set = new();
+
+        int[] items = { 1, 2, 3, 4, 4 };
+
+        set.AddRange(items);
+
+        set.Count.ShouldBe(4);
+        set.ShouldBe(new[] { 1, 2, 3, 4 }, ignoreOrder: true);
+    }
+
+    [Test]
+    public void When_adding_multiple_items_to_a_non_empty_set()
+    {
+        HashSet<int> set = new(new[] { 1, 2, 3 });
+
+        int[] items = { 2, 4, 4, 5 };
+
+        set.AddRange(items);
+
+        set.Count.ShouldBe(5);
+        set.ShouldBe(new[] { 1, 2, 3, 4, 5 }, ignoreOrder: true);
+    }
+
+    [Test]
+    public void When_adding_empty_items_to_a_non_empty_set()
+    {
+        HashSet<int> set = new(new[] { 1, 2, 3 });
+
+        int[] items = Array.Empty<int>();
+
+        set.AddRange(items);
+
+        set.Count.ShouldBe(3);
+        set.ShouldBe(new[] { 1, 2, 3 }, ignoreOrder: true);
+    }
+}
---
+++ Easy.Common/Extensions/HashSetExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace Easy.Common.Extensions;
+
+using System.Collections.Generic;
+using System.Diagnostics;
+
+/// <summary>
+/// Extension methods for <see cref="HashSet{T}"/>
+/// </summary>
+public static class HashSetExtensions
+{
+    /// <summary>
+    /// Adds the given <paramref name="items"/> to the given <paramref name="set"/>.
+    /// </summary>
+    [DebuggerStepThrough]
+    public static void AddRange<T>(this HashSet<T> set, IEnumerable<T> items)
+    {
+        foreach (T item in items)
+        {
+            set.Add(item);
+        }
+    }
+}
