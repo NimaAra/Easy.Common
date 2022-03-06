9817af9fc1234fc63b0395300b3ec6a8ecac2fd0
Nima Ara <NimaAra@users.noreply.github.com>
2022-03-06 17:16:55 +0000

    Introduce TypeLookup

---
+++ Easy.Common.Tests.Unit/TypeLookup/TypeLookupTests.cs
@@ -0,0 +1,36 @@
+﻿namespace Easy.Common.Tests.Unit.TypeLookup;
+
+using NUnit.Framework;
+using Shouldly;
+
+[TestFixture]
+internal sealed class TypeLookupTests
+{
+    [Test]
+    public void When_adding_and_resolving_values()
+    {
+        TypeLookup<string> lookup = new();
+
+        lookup.Count.ShouldBe(0);
+
+        lookup.Add<int>("A");
+        lookup.Add<string>("B");
+        lookup.Add<int>("C");
+        lookup.Add<double>(default);
+
+        lookup.Count.ShouldBe(4);
+
+        string value;
+        lookup.TryGet<int>(out value).ShouldBeTrue();
+        value.ShouldBe("C");
+
+        lookup.TryGet<string>(out value).ShouldBeTrue();
+        value.ShouldBe("B");
+
+        lookup.TryGet<double>(out value).ShouldBeTrue();
+        value.ShouldBe(default);
+
+        lookup.TryGet<byte>(out value).ShouldBeFalse();
+        value.ShouldBe(default);
+    }
+}
---
+++ Easy.Common/TypeLookup.cs
@@ -0,0 +1,66 @@
+﻿namespace Easy.Common;
+
+using System;
+using System.Threading;
+
+/// <summary>
+/// An abstraction for a fast lookup between a given type and a <typeparamref name="TValue"/>.
+/// </summary>
+public sealed class TypeLookup<TValue>
+{
+    // ReSharper disable once StaticMemberInGenericType
+    private static int TypeIndex = -1;
+    
+    private readonly object _sync = new();
+
+    private (TValue, bool)[] _values = new (TValue, bool)[100];
+
+    /// <summary>
+    /// Gets the number of registered items.
+    /// </summary>
+    public int Count { get; private set; }
+
+    /// <summary>
+    /// Adds the given <paramref name="value"/> against the given <typeparamref name="TKey"/>.
+    /// <remarks>Overwrites any existing value if key already exists.</remarks>
+    /// </summary>
+    public void Add<TKey>(TValue value)
+    {
+        lock (_sync)
+        {
+            int id = TypeKey<TKey>.Id;
+            if (id >= _values.Length)
+            {
+                Array.Resize(ref _values, id * 2);
+            }
+
+            _values[id] = new(value, true);
+            Count++;
+        }
+    }
+
+    /// <summary>
+    /// Attempts to resolve the value stored against the given <typeparamref name="TKey"/> if any.
+    /// </summary>
+    public bool TryGet<TKey>(out TValue value)
+    {
+        int id = TypeKey<TKey>.Id;
+        if (id >= _values.Length)
+        {
+            value = default;
+            return false;
+        }
+
+        (TValue val, bool hasVal) = _values[id];
+
+        value = val;
+        return hasVal;
+    }
+
+    // ReSharper disable once UnusedTypeParameter
+    private static class TypeKey<TKey>
+    {
+        // ReSharper disable once StaticMemberInGenericType
+        internal static readonly int Id = Interlocked.Increment(ref TypeIndex);
+    }
+}
