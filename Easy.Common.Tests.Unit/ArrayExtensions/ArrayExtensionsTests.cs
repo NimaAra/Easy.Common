341dd7c01bf1ae1676dec1eebd0c6ff5ea6c43b2
Nima Ara <Nima.Ara@Outlook.com>
2022-11-08 20:49:20 +0000

    Add ArrayExtensions

---
+++ Easy.Common.Tests.Unit/ArrayExtensions/ArrayExtensionsTests.cs
@@ -0,0 +1,98 @@
+﻿namespace Easy.Common.Tests.Unit.ArrayExtensions;
+
+using Easy.Common.Extensions;
+using NUnit.Framework;
+using Shouldly;
+
+[TestFixture]
+internal sealed class ArrayExtensionsTests
+{
+    [Test]
+    public void When_filling_and_copying_an_array()
+    {
+        int[] source = { 1, 2, 3, 4 };
+
+        int[] copied = source.FillAndCopy(42);
+        copied.ShouldBe(new[] {42, 42, 42, 42});
+
+        source.ShouldBe(new[] {1, 2, 3, 4});
+    }
+
+    [Test]
+    public void When_filling_and_copying_an_array_from_an_index()
+    {
+        int[] source = { 1, 2, 3, 4 };
+
+        int[] copied = source.FillAndCopy(24, 3);
+        copied.ShouldBe(new[] {1, 2, 3, 24});
+
+        source.ShouldBe(new[] {1, 2, 3, 4});
+    }
+
+    [Test]
+    public void When_filling_an_array()
+    {
+        int[] source = {1, 2, 3, 4};
+
+        int[] filled = source.Fill(42);
+
+        filled.ShouldBe(source);
+        filled.ShouldBe(new[] {42, 42, 42, 42});
+    }
+
+    [Test]
+    public void When_filling_an_array_from_an_index()
+    {
+        int[] source = {1, 2, 3, 4};
+
+        int[] filled = source.Fill(42, 3);
+
+        filled.ShouldBe(source);
+        filled.ShouldBe(new[] {1, 2, 3, 42});
+    }
+
+    [Test]
+    public void When_mapping_an_array()
+    {
+        int[] source = {1, 2, 3, 4};
+
+        int[] mapped = source.Map(x => x * 2);
+
+        mapped.ShouldBe(source);
+        mapped.ShouldBe(new[] {2, 4, 6, 8});
+    }
+
+    [Test]
+    public void When_mapping_an_array_from_an_index()
+    {
+        int[] source = {1, 2, 3, 4};
+
+        int[] mapped = source.Map(x => x * 2, 3);
+
+        mapped.ShouldBe(source);
+        mapped.ShouldBe(new[] {1, 2, 3, 8});
+    }
+
+    [Test]
+    public void When_mapping_and_copying_an_array()
+    {
+        int[] source = {1, 2, 3, 4};
+
+        int[] copied = source.MapAndCopy(x => x * 2);
+
+        copied.ShouldBe(new[] {2, 4, 6, 8});
+        source.ShouldBe(new[] {1, 2, 3, 4});
+    }
+
+    [Test]
+    public void When_mapping_and_copying_an_array_from_an_index()
+    {
+        int[] source = {1, 2, 3, 4};
+
+        int[] copied = source.MapAndCopy(x => x * 2, 3);
+
+        copied.ShouldBe(new[] {1, 2, 3, 8});
+
+        source.ShouldBe(new[] {1, 2, 3, 4});
+    }
+}
---
+++ Easy.Common/Extensions/ArrayExtensions.cs
@@ -0,0 +1,70 @@
+﻿namespace Easy.Common.Extensions;
+
+using System;
+using System.Diagnostics;
+using System.Linq;
+
+/// <summary>
+/// A set of extension methods for arrays.
+/// </summary>
+public static class ArrayExtensions
+{
+    /// <summary>
+    /// Returns a copy of the given <paramref name="array"/> and fills it with <paramref name="value"/>.
+    /// </summary>
+    [DebuggerStepThrough]
+    public static T[] FillAndCopy<T>(this T[] array, T value, int startIdx = 0)
+    {
+        T[] tmpArr = array.ToArray();
+
+        foreach (ref T item in tmpArr.AsSpan(startIdx))
+        {
+            item = value;
+        }
+
+        return tmpArr;
+    }
+
+    /// <summary>
+    /// Returns the given <paramref name="array"/> and fills it with <paramref name="value"/>.
+    /// </summary>
+    [DebuggerStepThrough]
+    public static T[] Fill<T>(this T[] array, T value, int startIdx = 0)
+    {
+        foreach (ref T item in array.AsSpan(startIdx))
+        {
+            item = value;
+        }
+
+        return array;
+    }
+
+    /// <summary>
+    /// Returns the given <paramref name="array"/> and fills it with values returned by the <paramref name="mapper"/>.
+    /// </summary>
+    [DebuggerStepThrough]
+    public static T[] Map<T>(this T[] array, Func<T, T> mapper, int startIdx = 0)
+    {
+        foreach (ref T item in array.AsSpan(startIdx))
+        {
+            item = mapper(item);
+        }
+
+        return array;
+    }
+
+    /// <summary>
+    /// Returns a copy of the given <paramref name="array"/> and fills it with values returned by the <paramref name="mapper"/>.
+    /// </summary>
+    [DebuggerStepThrough]
+    public static T[] MapAndCopy<T>(this T[] array, Func<T, T> mapper, int startIdx = 0)
+    {
+        T[] tmpArr = array.ToArray();
+        foreach (ref T item in tmpArr.AsSpan(startIdx))
+        {
+            item = mapper(item);
+        }
+
+        return tmpArr;
+    }
+}
