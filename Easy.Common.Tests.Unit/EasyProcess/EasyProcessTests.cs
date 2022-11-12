768133ecf2257e0750a3d8998cb729d01c957927
Nima Ara <Nima.Ara@Outlook.com>
2022-11-12 17:52:46 +0000

    Introduce EasyProcess

---
+++ Easy.Common.Tests.Unit/EasyProcess/EasyProcessTests.cs
@@ -0,0 +1,106 @@
+﻿namespace Easy.Common.Tests.Unit.EasyProcess;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Easy.Common.Extensions;
+using NUnit.Framework;
+using Shouldly;
+using EasyProcess = Easy.Common.EasyProcess;
+
+[TestFixture]
+internal sealed class EasyProcessTests
+{
+    private readonly List<string> _outputLines = new();
+    private readonly List<string> _errorLines = new();
+    
+    [SetUp]
+    public void TestSetUp()
+    {
+        _outputLines.Clear();
+        _errorLines.Clear();
+    }
+
+    [Test]
+    public async Task When_starting_a_process_with_no_errors()
+    {
+        using EasyProcess easyProc = new("ping", "-n 3 localhost");
+        
+        easyProc.OnOutput += (_, s) => _outputLines.Add(s);
+        easyProc.OnError += (_, s) => _errorLines.Add(s);
+
+        DateTime startTime = DateTime.Now;
+        
+        await easyProc.Start(CancellationToken.None);
+
+        DateTime endTime = DateTime.Now;
+
+        _errorLines.ShouldBeEmpty();
+        _outputLines.Count.ShouldBeGreaterThan(3);
+
+        _outputLines.Count(s => s.Contains("Reply from ::1:")).ShouldBe(3);
+
+        easyProc.HasExited.ShouldBeTrue();
+        easyProc.StartTime.ShouldBeInRange(startTime, endTime);
+        easyProc.ExitTime.ShouldBeInRange(startTime, endTime);
+        easyProc.ExecutionTime.ShouldBeInRange(1.Milliseconds(), 1.Minutes());
+        easyProc.ExitCode.ShouldBe(0);
+        easyProc.Id.ShouldBeGreaterThan(0);
+    }
+
+    [Test]
+    public async Task When_starting_a_process_with_errors()
+    {
+        using EasyProcess easyProc = new("dir", "localhost");
+        
+        easyProc.OnOutput += (_, s) => _outputLines.Add(s);
+        easyProc.OnError += (_, s) => _errorLines.Add(s);
+
+        DateTime startTime = DateTime.Now;
+        
+        await easyProc.Start(CancellationToken.None);
+
+        DateTime endTime = DateTime.Now;
+
+        _outputLines.ShouldBeEmpty();
+        _errorLines.Count.ShouldBe(1);
+
+        _errorLines[0].ShouldBe("dir: cannot access 'localhost': No such file or directory");
+
+        easyProc.HasExited.ShouldBeTrue();
+        easyProc.StartTime.ShouldBeInRange(startTime, endTime);
+        easyProc.ExitTime.ShouldBeInRange(startTime, endTime);
+        easyProc.ExecutionTime.ShouldBeInRange(1.Milliseconds(), 1.Minutes());
+        easyProc.ExitCode.ShouldBe(2);
+        easyProc.Id.ShouldBeGreaterThan(0);
+    }
+
+    [Test]
+    public async Task When_starting_a_process_then_cancelling_execution_after_a_while()
+    {
+        using EasyProcess easyProc = new("ping", "-t localhost");
+        
+        easyProc.OnOutput += (_, s) => _outputLines.Add(s);
+        easyProc.OnError += (_, s) => _errorLines.Add(s);
+
+        using CancellationTokenSource cts = new(2_000);
+
+        DateTime startTime = DateTime.Now;
+        
+        await easyProc.Start(cts.Token);
+
+        DateTime endTime = DateTime.Now;
+
+        _errorLines.ShouldBeEmpty();
+        _outputLines.Count(s => s.Contains("Reply from ::1:")).ShouldBeGreaterThan(1);
+
+        easyProc.HasExited.ShouldBeTrue();
+        easyProc.StartTime.ShouldBeInRange(startTime, endTime);
+        easyProc.ExitTime.ShouldBeInRange(startTime, endTime);
+        easyProc.ExecutionTime.ShouldBeInRange(1.Milliseconds(), 1.Minutes());
+        easyProc.ExitCode.ShouldBe(-1);
+        easyProc.Id.ShouldBeGreaterThan(0);
+    }
+}
---
+++ Easy.Common/EasyProcess.cs
@@ -0,0 +1,138 @@
+﻿namespace Easy.Common;
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Provides an abstraction to simplify executing a process asynchronously.
+/// </summary>
+public sealed class EasyProcess : IDisposable
+{
+    private readonly Process _process;
+    
+    private EasyProcess(ProcessStartInfo startInfo)
+    {
+        startInfo.UseShellExecute = false;
+        startInfo.CreateNoWindow = true;
+        startInfo.RedirectStandardOutput = true;
+        startInfo.RedirectStandardError = true;
+        
+        _process = new()
+        {
+            EnableRaisingEvents = true,
+            StartInfo = startInfo
+        };
+    
+        _process.OutputDataReceived += OnOutputData;
+        _process.ErrorDataReceived += OnErrorData;
+    }
+
+    /// <summary>
+    /// Creates an instance of the <see cref="EasyProcess"/>.
+    /// </summary>
+    public EasyProcess(string processPath, string args) : this(new ProcessStartInfo(processPath, args)) =>
+        Ensure.NotNullOrEmptyOrWhiteSpace(processPath);
+
+    /// <summary>
+    /// Creates an instance of the <see cref="EasyProcess"/>.
+    /// </summary>
+    public EasyProcess(FileInfo processPath, string args) : this(new ProcessStartInfo(processPath.FullName, args)) =>
+        Ensure.NotNull(processPath, nameof(processPath));
+
+    /// <summary>
+    /// Creates an instance of the <see cref="EasyProcess"/>.
+    /// </summary>
+    public EasyProcess(FileInfo processPath, DirectoryInfo workingDirectory, string args) : 
+        this(new ProcessStartInfo(processPath.FullName, args) { WorkingDirectory = workingDirectory.FullName })
+    {
+        ArgumentNullException.ThrowIfNull(processPath, nameof(processPath));
+        ArgumentNullException.ThrowIfNull(workingDirectory, nameof(workingDirectory));
+    }
+
+    /// <summary>
+    /// Gets the process Id.
+    /// </summary>
+    public int Id => _process.Id;
+
+    /// <summary>
+    /// Gets the value that the associated process specified when it terminated.
+    /// </summary>
+    public int ExitCode => _process.ExitCode;
+
+    /// <summary>
+    /// Gets the time that the associated process exited.
+    /// </summary>
+    public DateTime ExitTime => _process.ExitTime;
+
+    /// <summary>
+    /// Gets the time that the associated process was started.
+    /// </summary>
+    public DateTime StartTime => _process.StartTime;
+    
+    /// <summary>
+    /// Gets a value indicating whether the associated process has been terminated.
+    /// </summary>
+    public bool HasExited => _process.HasExited;
+
+    /// <summary>
+    /// Gets the execution time of the process.
+    /// </summary>
+    public TimeSpan ExecutionTime => _process.ExitTime - _process.StartTime;
+
+    /// <summary>
+    /// Raised on every process output line.
+    /// </summary>
+    public event EventHandler<string> OnOutput;
+    
+    /// <summary>
+    /// Raised on every process error line.
+    /// </summary>
+    public event EventHandler<string> OnError;
+
+    /// <summary>
+    /// Starts the process and publishes output and error lines if any as events.
+    /// </summary>
+    public async Task Start(CancellationToken cToken = default)
+    {
+        _process.Start();
+        _process.BeginOutputReadLine();
+        _process.BeginErrorReadLine();
+
+        try
+        {
+            await _process.WaitForExitAsync(cToken).ConfigureAwait(false);
+        } catch (TaskCanceledException)
+        {
+            _process.Kill(true);
+        }
+    }
+
+    /// <summary>
+    /// Releases all the resources used by this instance.
+    /// </summary>
+    public void Dispose()
+    {
+        _process.OutputDataReceived -= OnOutputData;
+        _process.ErrorDataReceived -= OnErrorData;
+        _process.Dispose();
+    }
+
+    private void OnOutputData(object sender, DataReceivedEventArgs e)
+    {
+        if (e.Data is not null)
+        {
+            OnOutput?.Invoke(this, e.Data);
+        }
+    }
+
+    private void OnErrorData(object sender, DataReceivedEventArgs e)
+    {
+        if (e.Data is not null)
+        {
+            OnError?.Invoke(this, e.Data);
+        }
+    }
+}
