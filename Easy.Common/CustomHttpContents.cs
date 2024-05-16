b571abc43f72f8a523f8311bd508baba46b0ddc4
Nima Ara <Nima.Ara@Outlook.com>
2024-05-16 18:59:51 +0100

    Remove JSONContent in favour of System.Net.Http.HttpContent.JsonContent

--- Easy.Common/CustomHttpContents.cs
+++ Easy.Common/CustomHttpContents.cs
@@ -3,24 +3,6 @@
 using System.Net.Http;
 
 /// <summary>
-/// Provides HTTP content based on a <c>JSON</c> string.
-/// </summary>
-public sealed class JSONContent : StringContent
-{
-    private const string JSONMime = "application/json";
-
-    /// <summary>
-    /// Creates an instance of the <see cref="JSONContent"/> with the default encoding of <see cref="System.Text.Encoding.UTF8"/>.
-    /// </summary>
-    public JSONContent(string jsonContent) : base(jsonContent, System.Text.Encoding.UTF8, JSONMime) {}
-
-    /// <summary>
-    /// Creates an instance of the <see cref="JSONContent"/> with the given <paramref name="encoding"/>.
-    /// </summary>
-    public JSONContent(string jsonContent, System.Text.Encoding encoding) : base(jsonContent, encoding, JSONMime) {}
-}
-
-/// <summary>
 /// Provides HTTP content based on an <c>XML</c> string.
 /// </summary>
 public sealed class XMLContent : StringContent
