97f82c1cdd5007bfe95ff1df7001b916748e596f
Nima Ara <Nima.Ara@Outlook.com>
2024-05-17 00:17:07 +0100

    Introduce JsonHelper

---
+++ Easy.Common.Tests.Unit/JsonHelper/JsonHelperTests.cs
@@ -0,0 +1,186 @@
+﻿namespace Easy.Common.Tests.Unit.JsonHelper;
+
+using System;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Shouldly;
+using JsonHelper = Easy.Common.JsonHelper;
+
+[TestFixture]
+internal sealed class JsonHelperTests
+{
+    [Test]
+    public void GivenMinifiedJson_WhenPrettifying_ThenShouldSucceed()
+    {
+        const string JSON = """{"name": "Foo", "age": 42}""";
+
+        JsonHelper.Prettify(JSON)
+            .ShouldBe(
+                """
+                {
+                  "name": "Foo",
+                  "age": 42
+                }
+                """);
+    }
+
+    [Test]
+    public void GivenJson_WhenDeserializingWithTemplate_ThenShouldSucceed()
+    {
+        const string JSON = """{"name": "Foo", "age": 42}""";
+
+        var template = new
+        {
+            name = string.Empty,
+            age = 0
+        };
+
+        var result = JsonHelper.DeserializeAs(template, JSON);
+
+        result.ShouldNotBeNull();
+        result.name.ShouldBe("Foo");
+        result.age.ShouldBe(42);
+    }
+
+    [Test]
+    public void GivenJson_WhenDeserializingWithTemplateAndDifferentCasing_ThenShouldSucceed()
+    {
+        const string JSON = """{"Name": "Foo", "AgE": 42}""";
+
+        var template = new
+        {
+            name = string.Empty,
+            age = 0
+        };
+
+        JsonSerializerOptions options = new() { PropertyNameCaseInsensitive = true };
+
+        var result = JsonHelper.DeserializeAs(template, JSON, options);
+
+        result.ShouldNotBeNull();
+        result.name.ShouldBe("Foo");
+        result.age.ShouldBe(42);
+    }
+
+    [Test]
+    public void GivenJson_WhenDeserializingFromStreamWithTemplate_ThenShouldSucceed()
+    {
+        const string JSON = """{"name": "Foo", "age": 42}""";
+        using MemoryStream ms = new(Encoding.UTF8.GetBytes(JSON));
+
+        var template = new
+        {
+            name = string.Empty,
+            age = 0
+        };
+
+        var result = JsonHelper.DeserializeAs(template, ms);
+
+        result.ShouldNotBeNull();
+        result.name.ShouldBe("Foo");
+        result.age.ShouldBe(42);
+    }
+
+    [Test]
+    public void GivenJson_WhenDeserializingFromReadOnlySpanCharWithTemplate_ThenShouldSucceed()
+    {
+        const string JSON = """{"name": "Foo", "age": 42}""";
+        ReadOnlySpan<char> jsonSpan = JSON.AsSpan();
+
+        var template = new
+        {
+            name = string.Empty,
+            age = 0
+        };
+
+        var result = JsonHelper.DeserializeAs(template, jsonSpan);
+
+        result.ShouldNotBeNull();
+        result.name.ShouldBe("Foo");
+        result.age.ShouldBe(42);
+    }
+
+    [Test]
+    public void GivenJson_WhenDeserializingFromReadOnlySpanByteWithTemplate_ThenShouldSucceed()
+    {
+        const string JSON = """{"name": "Foo", "age": 42}""";
+        ReadOnlySpan<byte> jsonSpan = Encoding.UTF8.GetBytes(JSON).AsSpan();
+
+        var template = new
+        {
+            name = string.Empty,
+            age = 0
+        };
+
+        var result = JsonHelper.DeserializeAs(template, jsonSpan);
+
+        result.ShouldNotBeNull();
+        result.name.ShouldBe("Foo");
+        result.age.ShouldBe(42);
+    }
+
+    [Test]
+    public void GivenJson_WhenDeserializingFromReadOnlyMemoryCharWithTemplate_ThenShouldSucceed()
+    {
+        const string JSON = """{"name": "Foo", "age": 42}""";
+        ReadOnlyMemory<char> jsonMemory = JSON.AsMemory();
+
+        var template = new
+        {
+            name = string.Empty,
+            age = 0
+        };
+
+        var result = JsonHelper.DeserializeAs(template, jsonMemory);
+
+        result.ShouldNotBeNull();
+        result.name.ShouldBe("Foo");
+        result.age.ShouldBe(42);
+    }
+
+    [Test]
+    public void GivenJson_WhenDeserializingFromReadOnlyMemoryByteWithTemplate_ThenShouldSucceed()
+    {
+        const string JSON = """{"name": "Foo", "age": 42}""";
+        ReadOnlyMemory<byte> jsonMemory = Encoding.UTF8.GetBytes(JSON).AsMemory();
+
+        var template = new
+        {
+            name = string.Empty,
+            age = 0
+        };
+
+        var result = JsonHelper.DeserializeAs(template, jsonMemory);
+
+        result.ShouldNotBeNull();
+        result.name.ShouldBe("Foo");
+        result.age.ShouldBe(42);
+    }
+
+    [Test]
+    public async Task GivenJson_WhenSerializingAndCompressingAndDeserializingFromCompressed_ThenShouldSucceed()
+    {
+        var payload = new
+        {
+            name = "Foo",
+            age = 42
+        };
+
+        using MemoryStream ms = new();
+        await JsonHelper.SerializeAndCompress(ms, payload);
+        
+        ms.Length.ShouldBe(41);
+
+        var template = payload;
+
+        ms.Position = 0;
+        var deserialized = await JsonHelper.DeserializeFromCompressed(ms, template);
+        
+        deserialized.ShouldNotBeNull();
+        deserialized.name.ShouldBe(payload.name);
+        deserialized.age.ShouldBe(payload.age);
+    }
+}
---
+++ Easy.Common/JsonHelper.cs
@@ -0,0 +1,87 @@
+﻿#pragma warning disable IDE0060
+
+namespace Easy.Common;
+
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Provides a set of methods to make working with JSON easier.
+/// </summary>
+public static class JsonHelper
+{
+    private static readonly JsonSerializerOptions _prettyOptions = new() { WriteIndented = true };
+
+    /// <summary>
+    /// Prettifies the given <paramref name="json"/>
+    /// </summary>
+    public static string Prettify(string json)
+    {
+        object? asObj = JsonSerializer.Deserialize<object>(json);
+        return JsonSerializer.Serialize(asObj, _prettyOptions);
+    }
+
+    /// <summary>
+    /// Deserializes payload from the given <paramref name="json"/> based on the given <paramref name="template"/>.
+    /// </summary>
+    public static T? DeserializeAs<T>(T template, string json, JsonSerializerOptions? options = null) =>
+        JsonSerializer.Deserialize<T>(json, options);
+
+    /// <summary>
+    /// Deserializes payload from the given <paramref name="json"/> based on the given <paramref name="template"/>.
+    /// </summary>
+    public static T? DeserializeAs<T>(T template, Stream json, JsonSerializerOptions? options = null) =>
+        JsonSerializer.Deserialize<T>(json, options);
+
+    /// <summary>
+    /// Deserializes payload from the given <paramref name="json"/> based on the given <paramref name="template"/>.
+    /// </summary>
+    public static T? DeserializeAs<T>(T template, ReadOnlySpan<char> json, JsonSerializerOptions? options = null) =>
+        JsonSerializer.Deserialize<T>(json, options);
+
+    /// <summary>
+    /// Deserializes payload from the given <paramref name="json"/> based on the given <paramref name="template"/>.
+    /// </summary>
+    public static T? DeserializeAs<T>(T template, ReadOnlySpan<byte> json, JsonSerializerOptions? options = null) =>
+        JsonSerializer.Deserialize<T>(json, options);
+
+    /// <summary>
+    /// Deserializes payload from the given <paramref name="json"/> based on the given <paramref name="template"/>.
+    /// </summary>
+    public static T? DeserializeAs<T>(T template, ReadOnlyMemory<byte> json, JsonSerializerOptions? options = null) =>
+        DeserializeAs(template, json.Span, options);
+
+    /// <summary>
+    /// Deserializes payload from the given <paramref name="json"/> based on the given <paramref name="template"/>.
+    /// </summary>
+    public static T? DeserializeAs<T>(T template, ReadOnlyMemory<char> json, JsonSerializerOptions? options = null) =>
+        DeserializeAs(template, json.Span, options);
+
+    /// <summary>
+    /// Deserializes payload from the given <paramref name="json"/> based on the given <paramref name="template"/>.
+    /// </summary>
+    public static async Task<T?> DeserializeFromCompressed<T>(Stream json, T template, JsonSerializerOptions? options = default)
+    {
+        await using GZipStream decompressor = new(json, CompressionMode.Decompress);
+        using MemoryStream decompressed = new();
+        await decompressor.CopyToAsync(decompressed);
+        decompressed.Position = 0;
+
+        return await JsonSerializer.DeserializeAsync<T>(decompressed, options);
+    }
+
+    /// <summary>
+    /// Serializes the given <paramref name="payload"/> then compresses the result and stores it into <paramref name="target"/>.
+    /// </summary>
+    public static async Task SerializeAndCompress<T>(
+        Stream target, T payload, JsonSerializerOptions? options = default, CompressionLevel level = CompressionLevel.Optimal)
+    {
+        await using GZipStream compressor = new(target, level, true);
+        await JsonSerializer.SerializeAsync(compressor, payload, options);
+
+        compressor.Close();
+    }
+}
